a55f922d60a6f802060df0a918b575d6cfe71c5b
MaximilianRieder <maximilian.rieder@t-online.de>
2025-04-03 13:52:07 +0200

    first commit

---
+++ src/main/java/systema/demo/artemis/remoteaddress/RemoteAddressPrinter.java
@@ -0,0 +1,11 @@
+/*
+* Company:   Systema GmbH
+* Copyright: (c) Copyright 2025 by Systema GmbH
+*                All Rights Reserved
+*/
+package main.java.systema.demo.artemis.remoteaddress;
+/**
+*
+* @author Maximilian Rieder
+*/public class RemoteAddressPrinter {
+}
